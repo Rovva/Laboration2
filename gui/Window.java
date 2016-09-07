15c424db082b8db45df2f5b545a67de36f64254d
Rovva <rovva@hostarn.se>
2016-09-07 14:30:01 +0200

    GUI är färdigt

--- gui/Window.java
+++ gui/Window.java
@@ -32,8 +32,10 @@ public class Window implements Observer, ActionListener {
 	private JLabel cache_label = new JLabel("Åldringstid:");
 	private JComboBox cache = new JComboBox();
 	private JButton select = new JButton("Hämta");
-	private JLabel prognos = new JLabel("Prognos för vald timma:");
-	private JLabel choosen_hour = new JLabel("Vald timma:");
+	private JLabel prognos_label = new JLabel("Prognos:");
+	private JLabel choosen_hour_label = new JLabel("Vald timma:");
+	private JLabel choosen_hour = new JLabel();
+	private JLabel temperature_label = new JLabel("Temperatur:");
 	private JLabel temperature = new JLabel();
 	
 	
@@ -55,7 +57,13 @@ public class Window implements Observer, ActionListener {
 		contentPane.add(cache_label);
 		contentPane.add(cache);
 		contentPane.add(select);
-
+		
+		contentPane.add(prognos_label);
+		contentPane.add(choosen_hour_label);
+		contentPane.add(choosen_hour);
+		contentPane.add(temperature_label);
+		contentPane.add(temperature);
+		
 		city_choose.addItem("Skellefteå");
 		Date date = new Date();
 		current_date.setText(date.toString());
@@ -87,6 +95,20 @@ public class Window implements Observer, ActionListener {
 		layout.putConstraint(SpringLayout.NORTH, select, 0, SpringLayout.NORTH, current_date);
 		layout.putConstraint(SpringLayout.WEST, select, 10, SpringLayout.EAST, current_date);
 		
+		layout.putConstraint(SpringLayout.NORTH, prognos_label, 60, SpringLayout.SOUTH, cache);
+		layout.putConstraint(SpringLayout.WEST, prognos_label, 2, SpringLayout.WEST, contentPane);
+		
+		layout.putConstraint(SpringLayout.NORTH, choosen_hour_label, 2, SpringLayout.SOUTH, prognos_label);
+		layout.putConstraint(SpringLayout.WEST, choosen_hour_label, 2, SpringLayout.WEST, contentPane);
+		
+		layout.putConstraint(SpringLayout.NORTH, choosen_hour, 2, SpringLayout.SOUTH, prognos_label);
+		layout.putConstraint(SpringLayout.WEST, choosen_hour, 2, SpringLayout.EAST, choosen_hour_label);
+		
+		layout.putConstraint(SpringLayout.NORTH, temperature_label, 2, SpringLayout.SOUTH, choosen_hour_label);
+		layout.putConstraint(SpringLayout.WEST, temperature_label, 2, SpringLayout.WEST, contentPane);
+		
+		layout.putConstraint(SpringLayout.NORTH, temperature, 2, SpringLayout.SOUTH, choosen_hour);
+		layout.putConstraint(SpringLayout.WEST, temperature, 2, SpringLayout.EAST, temperature_label);
 		
 	}
 
