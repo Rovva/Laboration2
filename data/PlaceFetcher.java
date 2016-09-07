19b4c2159d9eeb2dfaf96200b2e371dd7828f38b
Beldrama <roininin4@hotmail.com>
2016-09-07 17:29:53 +0200

    DomParser to PlaceFetcher
    Jodå

--- data/PlaceFetcher.java
+++ data/PlaceFetcher.java
@@ -2,50 +2,65 @@ package data;
 
 import java.util.ArrayList;
 import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
 import javax.xml.parsers.*;
 import java.io.*;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.NodeList;
 
 public class PlaceFetcher {
 
 	File file;
-	String filepath;
 	ArrayList cities, altitude, latitude, longitude;
 	Document placesDoc;
+	DocumentBuilderFactory factory;
 	
-	public PlaceFetcher(String file) {
-		filepath = file;
-		cities  = new ArrayList();
-		altitude  = new ArrayList();
-		longitude  = new ArrayList();
+	public PlaceFetcher() {
+		cities = new ArrayList();
+		longitude = new ArrayList();
+		latitude = new ArrayList();
+		altitude = new ArrayList();
+		factory = DocumentBuilderFactory.newInstance();
 		// TODO Auto-generated constructor stub
 	}
 
 	void updatePlaces() {
+		
+		clearCities();
+		clearAltitude();
+		clearLatitude();
+		clearLongitude();
+		
 		try {
-			file = new File(filepath);
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			placesDoc = dBuilder.parse(filepath);
-			placesDoc.getDocumentElement().normalize();
-			NodeList nList = placesDoc.getElementsByTagName("locality");
-			for(int i = 0; i < nList.getLength(); i++) {
-				Node nNode = nList.item(i);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					this.addCities(eElement.getAttribute("name"));
-					this.addAltitude(eElement.getAttribute("altitude"));
-					this.addLongitude(eElement.getAttribute("longitude"));
-					this.addLatitude(eElement.getAttribute("latitude"));
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse("places.xml");
+			NodeList localityList = doc.getElementsByTagName("locality");
+			NodeList locationList = doc.getElementsByTagName("location");
+			for(int i = 0; i < localityList.getLength(); i++){
+				Node l = localityList.item(i);
+				if (l.getNodeType()==Node.ELEMENT_NODE){
+					Element locality = (Element) l; 
+					cities.add(locality.getAttribute("name"));
 				}
 			}
-		} catch (Exception e) {
-	         e.printStackTrace();
-	    }
+			for(int i = 0; i < locationList.getLength(); i++){
+				Node l = locationList.item(i);
+				if (l.getNodeType()==Node.ELEMENT_NODE){
+					Element location = (Element) l; 
+					longitude.add(location.getAttribute("longitude"));
+					latitude.add(location.getAttribute("latitude"));
+					altitude.add(location.getAttribute("altitude"));
+				}
+			}
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 	
 	void addCities(String city) {
