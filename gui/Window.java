e8a408debc9c02b7e930eec0fcbf7748706f7888
Rovva <rovva@hostarn.se>
2016-09-07 14:14:43 +0200

    Positionerat ut knappar osv.

--- gui/Window.java
+++ gui/Window.java
@@ -8,6 +8,7 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.util.Date;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -23,9 +24,17 @@ public class Window implements Observer, ActionListener {
 
 	int size_x = 500, size_y = 400;
 	private JLabel city_label = new JLabel("Välj stad:");
+	private JLabel hour_label = new JLabel("Välj timma:");
+	private JLabel date_label = new JLabel("Dagens datum:");
+	private JLabel current_date = new JLabel();
 	private JComboBox city_choose = new JComboBox();
-	private JLabel time_from, time_to, altitude, latitude, longitude, city,
-		temperature = new JLabel();
+	private JComboBox hour_choose = new JComboBox();
+	private JLabel cache_label = new JLabel("Åldringstid:");
+	private JComboBox cache = new JComboBox();
+	private JButton select = new JButton("Hämta");
+	private JLabel prognos = new JLabel("Prognos för vald timma:");
+	private JLabel choosen_hour = new JLabel("Vald timma:");
+	private JLabel temperature = new JLabel();
 	
 	
 	public Window() {
@@ -39,15 +48,46 @@ public class Window implements Observer, ActionListener {
 		contentPane.setLayout(layout);
 		contentPane.add(city_label);
 		contentPane.add(city_choose);
-		contentPane.add(time_from);
-		contentPane.add(time_to);
-		contentPane.add(altitude);
-		contentPane.add(latitude);
-		contentPane.add(longitude);
-		contentPane.add(city);
-		contentPane.add(temperature);
+		contentPane.add(hour_choose);
+		contentPane.add(hour_label);
+		contentPane.add(date_label);
+		contentPane.add(current_date);
+		contentPane.add(cache_label);
+		contentPane.add(cache);
+		contentPane.add(select);
+
+		city_choose.addItem("Skellefteå");
+		Date date = new Date();
+		current_date.setText(date.toString());
 		
 		layout.putConstraint(SpringLayout.NORTH, city_label, 2, SpringLayout.NORTH, contentPane);
+		layout.putConstraint(SpringLayout.WEST, city_label, 2, SpringLayout.WEST, contentPane);
+
+		layout.putConstraint(SpringLayout.NORTH, hour_label, 2, SpringLayout.NORTH, contentPane);
+		layout.putConstraint(SpringLayout.WEST, hour_label, 22, SpringLayout.EAST, city_choose);
+		
+		layout.putConstraint(SpringLayout.NORTH, city_choose, 5, SpringLayout.SOUTH, city_label);
+		layout.putConstraint(SpringLayout.WEST, city_choose, 2, SpringLayout.WEST, contentPane);
+
+		layout.putConstraint(SpringLayout.NORTH, hour_choose, 5, SpringLayout.SOUTH, hour_label);
+		layout.putConstraint(SpringLayout.WEST, hour_choose, 22, SpringLayout.EAST, city_choose);
+
+		layout.putConstraint(SpringLayout.NORTH, date_label, 2, SpringLayout.NORTH, contentPane);
+		layout.putConstraint(SpringLayout.WEST, date_label, 10, SpringLayout.EAST, hour_label);
+		
+		layout.putConstraint(SpringLayout.NORTH, current_date, 2, SpringLayout.SOUTH, date_label);
+		layout.putConstraint(SpringLayout.WEST, current_date, 10, SpringLayout.EAST, hour_label);
+
+		layout.putConstraint(SpringLayout.NORTH, cache_label, 2, SpringLayout.SOUTH, city_choose);
+		layout.putConstraint(SpringLayout.WEST, cache_label, 2, SpringLayout.WEST, contentPane);
+		
+		layout.putConstraint(SpringLayout.NORTH, cache, 2, SpringLayout.SOUTH, cache_label);
+		layout.putConstraint(SpringLayout.WEST, cache, 2, SpringLayout.WEST, contentPane);
+		
+		layout.putConstraint(SpringLayout.NORTH, select, 0, SpringLayout.NORTH, current_date);
+		layout.putConstraint(SpringLayout.WEST, select, 10, SpringLayout.EAST, current_date);
+		
+		
 	}
 
 	@Override
--- main/Main.java
+++ main/Main.java
@@ -1,13 +1,13 @@
 package main;
 
 import gui.Window;
-import controller.EventHandler;
+//import controller.EventHandler;
 import data.Fetcher;
 
 public class Main {
 	public static void main(String[] args) {
-		Fetcher fetcher = new Fetcher("./places.xml");
-		EventHandler control = new EventHandler();
+		//Fetcher fetcher = new Fetcher("./places.xml");
+		//EventHandler control = new EventHandler();
 		Window window = new Window();
 	}
 }
