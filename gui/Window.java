3abcc26b93af6c7a5d0a26bb29ed22933edd61c7
Rovva <rovva@hostarn.se>
2016-09-07 13:10:51 +0200

    Gjort lite på gui

--- gui/Window.java
+++ gui/Window.java
@@ -8,6 +8,7 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseListener;
+import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JButton;
@@ -23,17 +24,30 @@ public class Window implements Observer, ActionListener {
 	int size_x = 500, size_y = 400;
 	private JLabel city_label = new JLabel("Välj stad:");
 	private JComboBox city_choose = new JComboBox();
-	private JLabel time_from, time_to, altitude, latitude, longitude, city, humidity,
+	private JLabel time_from, time_to, altitude, latitude, longitude, city,
 		temperature = new JLabel();
 	
 	
-	public Window(Fetcher data) {
+	public Window() {
 		JFrame frame = new JFrame("Weather");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(size_x, size_y);
 		frame.setVisible(true);
 		SpringLayout layout = new SpringLayout();
 		Container contentPane = frame.getContentPane();
+
+		contentPane.setLayout(layout);
+		contentPane.add(city_label);
+		contentPane.add(city_choose);
+		contentPane.add(time_from);
+		contentPane.add(time_to);
+		contentPane.add(altitude);
+		contentPane.add(latitude);
+		contentPane.add(longitude);
+		contentPane.add(city);
+		contentPane.add(temperature);
+		
+		layout.putConstraint(SpringLayout.NORTH, city_label, 2, SpringLayout.NORTH, contentPane);
 	}
 
 	@Override
@@ -47,8 +61,6 @@ public class Window implements Observer, ActionListener {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	private
 	
 	
 }
